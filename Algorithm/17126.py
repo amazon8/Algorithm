ecb1494309b31a82879c8c0e4f8cd0d6f2242eb9
yangbyeonghun <21900428@handong.edu>
2021-11-22 03:33:15 +0000

    인덱스 0미만 일때 추가

--- Algorithm/17126.py
+++ Algorithm/17126.py
@@ -1,5 +1,10 @@
-import math
+'''
+시작 : 11:25
+'''
 
+import math, time
+
+start = time.time()
 n = int(input())
 m = math.floor(math.sqrt(n))
 a = [50001 for i in range(n+1)]
@@ -10,8 +15,9 @@ for i in range(1, m+1):
   for j in range(1, n+1):
     if math.sqrt(j)%1 ==0:
       a[j] = 1
-    else:
+    elif (j-i**2)>0:
       a[j] = min(a[j], a[j-i**2]+1)
 
-
 print(a[n])
+end = time.time()
+print("{:.5f} sec".format(end-start))
