40ca53d2440cb8a5136c87f9020846dbbdd5b19e
yangbyeonghun <21900428@handong.edu>
2021-11-25 03:48:14 +0000

    문자열

---
+++ Greedy/12904.py
@@ -0,0 +1,18 @@
+s = input()
+t = list(input())
+num = len(t)-len(s)
+
+# 거꾸로 하나씩 제거
+for i in range(num):
+  if t[-1] == 'A':
+    t.pop()
+  else:
+    t.pop()
+    t.reverse()
+
+print(t, s)
+# 비교
+if "".join(t)==s:
+  print(1)
+else:
+  print(0)
