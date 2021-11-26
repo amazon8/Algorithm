bb61f7eb639d6ba9d065696e39cb3a3126b8358f
yangbyeonghun <21900428@handong.edu>
2021-11-26 03:02:31 +0000

    틀림

---
+++ dynamic/14606.py
@@ -0,0 +1,18 @@
+height = int(input()) 
+sum = [0 for i in range(height+1)]
+if height >= 1:
+  sum[1] = 1
+if height >=2:
+  sum[2] = 1
+if height >= 3:
+  sum[3] = 3
+
+
+# heigt까지 값 채우기
+for i in range(4, height+1):
+  if i % 2 ==0:
+    sum[i] = (i//2) * (i//2) + sum[i//2] * 2
+  else:
+    sum[i] = int(i//2) * (int(i//2)+1) + sum[int(i//2)] + sum[int(i//2)+1]
+
+print(sum[height])
