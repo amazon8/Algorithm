62c2d582e5d6aa6d72d3ab4a854f947a33a62b07
yangbyeonghun <21900428@handong.edu>
2021-11-29 16:12:04 +0000

    예외 부분 존재

--- greedy/1493.py
+++ greedy/1493.py
@@ -1,13 +1,29 @@
+import math
+
 length, wid, hei = map(int, input().split())
 num = int(input())
 cube = [[0 for col in range(2)] for row in range(num)]
 for i in range(num):
-  a[i][0], a[i][1] = map(int, input().split())
+  cube[i][0], cube[i][1] = map(int, input().split())
+count = 0
 
 # 큐브 부피만큼 빼는 과정
 vol = length*wid*hei
 for i in reversed(range(num)):
   cube_side = 2**cube[i][0]
-  if cube_side <= max(length, wid, hei):
-    if vol // cube_side*cube_side*cube_side <= cube[i][1]:
-      vol = vol % (2**cube[i][0])  
+  cube_vol = math.pow(cube_side, 3)
+  if cube_side <= min(length, wid, hei):
+    print(i)
+    if vol // cube_vol <= cube[i][1]:
+      count += int(vol //cube_vol) 
+      vol = vol % cube_vol 
+    else:
+      vol = vol - (cube_vol*cube[i][1])
+      count += cube[i][1]
+  if vol == 0:
+    break
+
+if vol ==0 :
+  print(count)
+else:
+  print(-1)    
