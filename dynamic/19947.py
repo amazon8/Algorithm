c98b54f66c1a63055d81c665567b1b5fa9304f1e
yangbyeonghun <21900428@handong.edu>
2021-11-30 06:48:12 +0000

    수정

--- dynamic/19947.py
+++ dynamic/19947.py
@@ -1,13 +1,15 @@
 money, year = map(int, input().split())
 num = [0 for i in range(3)]
 num[0] = year//5
-year = year//5
+if num[0]!=0:
+  year = year//5
 num[1] = year//3
-year = year//3
+if num[1]!=0:
+  year = year//3
 num[2] = year
 
 # 이자 추가
-for i in reversed(3):
+for i in reversed(num):
   for j in range(i):
     if i == 2:
       money += int(money*0.05)
