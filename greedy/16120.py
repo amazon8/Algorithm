be335f21a8eb0fcd2c5085e4be86c87b037c72e4
yangbyeonghun <21900428@handong.edu>
2021-12-01 14:03:48 +0000

    최종

--- greedy/16120.py
+++ greedy/16120.py
@@ -6,7 +6,7 @@ result=[]
 
 # ppap 문자열 p로 바꿈
 for i in range(len(str)):
-  if len(result) >= 2 and str[i] == "A" and result[-2] == "P" and result[-1] == "P" and str[i+1] == "P":
+  if len(result) >= 2 and i <= len(str)-2 and str[i] == "A" and result[-2] == "P" and result[-1] == "P" and str[i+1] == "P":
     del result[-2:]
   else:
     result.append(str[i])
