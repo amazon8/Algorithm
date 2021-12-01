f0560769fab22132104a72fec049fee79b2c2754
yangbyeonghun <21900428@handong.edu>
2021-12-01 03:37:15 +0000

    수정

--- greedy/16120.py
+++ greedy/16120.py
@@ -1,13 +1,23 @@
-import sys
+str = list(input())
+result=[]
+i=0
 
-str = sys.stdin.readline()
-str = str.strip()
+# 스택으로 ppap제거
+while True:
+  if i<=len(str)-4 and str[i]=="P" and str[i+1]=="P" and str[i+2]=="A" and str[i+3]=="P":
+    result.append(str[i])
+    i+=4
+  else:
+    result.append(str[i])
+    i+=1
+  if 'PPAP' in "".join(result):
+    for j in range(3):
+      result.pop()
+  if i == len(str):
+    break
 
-# ppap 있는 경우 p로 치환
-while 'PPAP' in str:
-  str = str.replace('PPAP', 'P')
-
-if str == 'P':
-  print("PPAP")
+print(result)
+if ''.join(result)=='P':
+  print('PPAP')
 else:
-  print("NP")
+  print('NP')
