5ba3faf0bb836bdb7eee3258e31c07974d1755c3
yangbyeonghun <21900428@handong.edu>
2021-12-02 03:00:12 +0000

    a

--- dynamic/19947.py
+++ dynamic/19947.py
@@ -1,15 +1,20 @@
+'''
+1. 다이나믹으로 풀었는데 어디서 틀렸는지 모르겠음...
+--> 80% 쯤에서 틀리다는데 반례가 뭐지,,,? 왠만한거는 다 넣어 봤는데
+'''
+
 m, year = map(int, input().split())
 money = [0 for i in range(year+1)]
 money[0] = m 
 
 # 연도별 이자 추가
-for i in range(1, 6, 2):
-  for j in range(1, year+1):
-    if i == 1:
-      money[j] = max(money[j], int(money[j-i]*1.05))
-    elif i ==3 and j-i>=0:
-      money[j] = max(money[j], int(money[j-i]*1.2))
-    elif i==5 and j-i>=0:
-      money[j] = max(money[j], int(money[j-i]*1.35)) 
+for term in range(1, 6, 2):
+  for now_year in range(1, year+1):
+    if term == 1:
+      money[now_year] = max(money[now_year], int(money[now_year-term]*1.05))
+    elif term == 3 and now_year - term >= 0:
+      money[now_year] = max(money[now_year], int(money[now_year-term]*1.2))
+    elif term == 5 and now_year-term >= 0:
+      money[now_year] = max(money[now_year], int(money[now_year-term]*1.35)) 
 
 print(money[year])
