8f9d6d2fe3cfb16cf33b99eb125462825b15a847
yangbyeonghun <21900428@handong.edu>
2021-12-04 06:57:45 +0000

    다이나믹 문제에서 어떤 풀이의 틀에 갇혀 여러번 푼 문제

--- dynamic/1463.py
+++ dynamic/1463.py
@@ -1,21 +1,14 @@
-import sys
-sys.setrecursionlimit(1000020)
-
 num = int(input())
-nums = [1000000 for i in range(num+1)]
+divs = [2, 3]
+nums = [33334 for i in range(num+1)]
 nums[1] = 0
 
-# 최소값 찾기
-def mins(now):
-  result = int
-  if nums[now]!=1000000:
-    return nums[now]
-  else:
-    nums[now] = min(mins(now-1)+1, nums[now])
-    if int(now/2) == now/2:
-      nums[now] = min(nums[now], mins(now//2)+1)
-    if int(now/3) == now/3:
-      nums[now] = min(nums[now], mins(now//3)+1)
-    return nums[now]
-    
-print(mins(num))
+# 숫자를 최소값으로 갱신
+for now in range(2, num+1):
+  if int(now/2) == now/2:
+    nums[now] = min(nums[now], nums[now//2]+1)
+  if int(now/3) == now/3:
+    nums[now] = min(nums[now], nums[now//3]+1)
+  nums[now] = min(nums[now], nums[now-1]+1)
+
+print(nums[num])
