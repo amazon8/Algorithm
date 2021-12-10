35deed696889ac16dcf926baf501d862f4466ed3
yangbyeonghun <21900428@handong.edu>
2021-12-10 02:59:01 +0000

    완료

--- dynamic/1912.py
+++ dynamic/1912.py
@@ -1,12 +1,15 @@
+'''
+1. array를 전, 전+지금, 지금 세개 비교 후 max 넣기
+--> 앞뒤로는 작은데 전제가 큰 경우가 발생
+2. array를 전, 전+지금, 지금 비교 --> 지금이 max가 아니면 전+지금 값을 array에 넣기
+'''
 num = int(input())
 array = list(map(int, input().split()))
 
 # 연속된 합의 최대로 채워가기
-for now in range(1, len(array)-1):
-  if max(array[now-1], array[now-1]+array[now], array[now]) == (array[now-1]+array[now]):
-    array[now] = max(array[now-1], array[now-1]+array[now], array[now])
-  else:
-    array[now] = 0
-  print(array)
+for now in range(1, len(array)):
+  M = max(array[now-1], array[now-1]+array[now], array[now])
+  if M == (array[now-1]+array[now]) or M == array[now-1]:
+    array[now] = array[now-1] + array[now]
 
 print(max(array))
