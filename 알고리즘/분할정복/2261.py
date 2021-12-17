2917571e0252fdec4ef818bf9adf9359412bd1c5
yangbyeonghun <21900428@handong.edu>
2021-12-17 04:21:59 +0000

    틀림,

--- 알고리즘/분할정복/2261.py
+++ 알고리즘/분할정복/2261.py
@@ -1,6 +1,8 @@
 '''
 1. 병합정렬 --> 모든 경우 비교x, 개수가 홀수인 경우 비교 불가
-2. 버블정렬로 가능 할 거 같은데,,
+2. 버블정렬로 가능 할 거 같은데,, --> 모든 경우 비교x 
+ex) [1,-2] [2,9] [3,11] [4,1]
+3. 분할정복, 스위핑을 써야할듯,,,,
 '''
 num = int(input())
 location = []
@@ -11,6 +13,7 @@ for i in range(num):
 location.sort()
 dif_x = abs(location[0][0]-location[1][0])
 dif_y = abs(location[0][1]-location[1][1])
+print(dif_x, dif_y)
 
 
 # 버블정렬 방식
