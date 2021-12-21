117e1fb3cf58c7714566775167f15ea8df8baf64
yangbyeonghun <21900428@handong.edu>
2021-12-21 04:46:18 +0000

    중간 단계

--- 알고리즘/분할정복/1517.py
+++ 알고리즘/분할정복/1517.py
@@ -1,45 +1,35 @@
-'''
-1. 버블정렬 풀이
-  recursion error가 표시됨,,, 재귀오류가 왜 일어나는 걸까,,?
---> 시간복잡도가 1초보다 훨씬 초과 됨, 대략적으로는 시간을 측정하고 알고리즘 방향을 결정해야 할듯
-2. 병합 정렬 알고리즘 --> 메모리 초과,, 흠,,
-'''
-
 num = int(input())
-arr = list(map(int, input().split()))
-left = []
-right = []
-swap_c = 0; mid = 0
+array = list(map(int, input().split()))
+swap_c = 0
 
-# 분할 함수
-def split(array):
-  if len(array) ==1:
-    return array
-  mid = len(array)//2
-  left = split(array[:mid])
-  right = split(array[mid:])
-  return merge(left, right)
-
-# 병합 함수
-def merge(left, right):
+# 병합정렬로 swap 카운트
+def merge_sort(start, end):
   global swap_c
-  final_array = []
+  temp = []
+  if start == end:
+    return
+  mid = (start+end)//2
+  left = merge_sort(start, mid)
+  right = merge_sort(mid+1, end)
+  
   left_index = 0; right_index = 0
-  while left_index < len(left) and right_index < len(right):
-    if left[left_index] > right[right_index]:
-      swap_c += 1
-      final_array.append(right[right_index])
+  while left_index < mid and right_index < end:
+    if array[mid+right_index] < array[start+left_index]:
+      swap_c += mid-left_index
+      print(start, end, array, left_index, right_index, swap_c)
+      temp.append(array[mid+right_index])
       right_index += 1
     else:
-      final_array.append(left[left_index])
+      temp.append(array[start+left_index])
       left_index += 1
 
-  if right_index == len(right):    
-    final_array.extend(left)
-  else: 
-    final_array.extend(right)
+  if left_index == mid:
+    temp.extend(array[right_index:end+1])
+  else:
+    temp.extend(array[left_index:mid])
 
-  return final_array
+  for index in range(len(temp)):
+    array[start+index] = temp[index]
 
-result = split(arr)
+merge_sort(0, num-1)
 print(swap_c)
