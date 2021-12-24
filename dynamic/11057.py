452530f2591be0a3014b6c4c8548aa679c0ce0a6
yangbyeonghun <21900428@handong.edu>
2021-12-24 03:17:51 +0000

    완료

--- dynamic/11057.py
+++ dynamic/11057.py
@@ -5,7 +5,6 @@ array[1] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 # 끝에 맞혀서 개수 추가해가기
 for now_line in range(2, length+1):
   for now_end in range(10):
-    for plus_index in range(now_end+1):
-      array[now_line][now_end] += array[now_line-1][plus_index]
+      array[now_line][now_end] += sum(array[now_line-1][:now_end+1])
 
 print(sum(array[length])%10007)
