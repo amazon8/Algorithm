3eaf8ce74bb30cc17e1e01ab76bfeefd5e8ce004
yangbyeonghun <21900428@handong.edu>
2021-12-28 16:57:46 +0000

    완료

---
+++ 알고리즘/탐색/1654.py
@@ -0,0 +1,21 @@
+now_lan, need_lan = map(int, input().split())
+lan = []
+for i in range(now_lan):
+  lan.append(int(input()))
+lan.sort()
+start = 1; end = lan[-1]; mid = 0; 
+
+
+# 0부터 max길이 까지 나오는 랜선 길이 비교 --> 최대 길이 찾기
+while start <= end :
+  mid = (start+end)//2
+  nums = 0
+  for now_length in lan:
+    nums += now_length // mid
+  
+  if nums >= need_lan:
+    start = mid+1
+  else:
+    end = mid-1
+
+print(end)
