45faac5c6d456c1b5feeccb46e0c419d5276105a
yangbyeonghun <21900428@handong.edu>
2022-01-03 09:13:22 +0000

    완료

--- 알고리즘/수학/1929.py
+++ 알고리즘/수학/1929.py
@@ -1,4 +1,11 @@
-1
+'''
+1. 소수 판별 함수 --> 시간 초과
+  --> 시간 복잡도를 로그로 바꿔야 할듯,,
+2. 해당 수에 확인하는 수를 루트 n 까지로 잡으니 시간초과 해결
+♨ 시간초과의 경우 N --> logN 뿐만 아니라 루트 N(math.sqrt)으로도 줄일 수 있음!!
+'''
+import math
+
 start, end = map(int, input().split())
 
 # 소수 판별
@@ -8,7 +15,9 @@ def check_prime(num):
   if num == 2:
     print(num)
     return
-  for divide in range(2, num//2+1):
+  end = math.floor(math.sqrt(num))
+
+  for divide in range(2, end+1):
     if num % divide ==0:
       return
   print(num)
