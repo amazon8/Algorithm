27fb27a7b8d3a4ad3b8f193e79c1d901ce589e44
yangbyeonghun <21900428@handong.edu>
2022-01-05 15:26:43 +0000

    수정!

--- 알고리즘/완전탐색/1107.py
+++ 알고리즘/완전탐색/1107.py
@@ -1,27 +1,36 @@
+'''
+1. 왜 틀렸는지 모르겠네,,,,
+  --> 경계가 되는 부분에서 예외가 발생하는지 확인!!
+'''
 final_channel = int(input())
-a = input()
-broken = list(map(int, input().split()))
+broken_num = int(input())
+if broken_num != 0:
+  broken = list(map(int, input().split()))
+else:
+  broken = []
+if final_channel == 100:
+  print(0)
+  exit()
+if broken_num == 10:
+  print(abs(final_channel-100))
+  exit()
+
 channel = 0
-optimal_channel = 0
+optimal_channel = 10000001
 
-# 버튼 9999까지 확인
-while channel <= 9999:
-
-  int_channel = channel
-  list_channel = list(map(int, str(channel)))
+def possible_channel(channels):
+  global broken
+  list_channel = list(map(int, str(channels)))
   for index in range(len(list_channel)):
     if list_channel[index] in broken:
-      break
-    else:
-      if index == len(list_channel)-1 and abs(optimal_channel - final_channel) > abs(int_channel-final_channel):
-        optimal_channel = int_channel
+      return False
+  return channels
 
-  channel += 1
+# 채널값의 차이가 최소인 값 찾기
+while channel <= 1000000:
+  res_channel = possible_channel(channel)
+  if res_channel != False and abs(optimal_channel-final_channel)+len(str(optimal_channel))>abs(res_channel-final_channel)+len(str(res_channel)):
+    optimal_channel = res_channel
+  channel += 1  
 
-
-shift = abs(final_channel-optimal_channel)
-final_channel = list((str(final_channel)))
-shift += len(final_channel)
-
-print(shift)
-
+print(min((abs(optimal_channel-final_channel)+len(str(optimal_channel))), abs(final_channel-100)))
