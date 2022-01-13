c9ee89d053df781a585c1829b5f8607868bfc044
yangbyeonghun <21900428@handong.edu>
2022-01-13 03:10:33 +0000

    완료

--- 알고리즘/완전탐색/1697.py
+++ 알고리즘/완전탐색/1697.py
@@ -1,19 +1,16 @@
 '''
-1. bfs보다는 dp문제 같은데,,,,
+1. bfs보다는 dp문제 같은데,,,, 
+  --> bfs로 풀 수는 있는데 dp로 푸는게 나을 듯
 '''
 
 subin, brother = map(int, input().split())
-spot = [ i for i in range(brother+2)]
-print(spot)
-for index in range(subin+1):
-  spot[index] = 0
-print(spot)
+spot = [abs(subin-i) for i in range(brother+3)]
 
 # 수빈부터 동생있는 곳까지 dp
-for now in range(subin+1, brother+1):
-  spot[now] = min(spot[now-1]+1, spot[now+1]-1)
-  if now % 2 == 0 and now >= subin*2:
+for now in range(subin+1, brother+2):
+  spot[now] = min(spot[now-1]+1, spot[now+1]+1)
+  if now % 2 == 0 :
     spot[now] = min(spot[now], spot[now//2]+1)
+    spot[now-1] = min(spot[now-1], spot[now]+1)
 
-print(spot)
 print(spot[brother])
