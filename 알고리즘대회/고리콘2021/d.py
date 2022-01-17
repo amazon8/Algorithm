47b1855ed8fc7e537bd98f5567d1f5830c50a371
yangbyeonghun <21900428@handong.edu>
2022-01-17 13:47:04 +0000

    수정

--- 알고리즘대회/고리콘2021/d.py
+++ 알고리즘대회/고리콘2021/d.py
@@ -1,1 +1,14 @@
-print(5&6&7&15)
+input()
+num = list(map(int, input().split()))
+count = [0 for i in range(25)]
+
+# 자리별 1의 개수 확인
+for now_num in num:
+  temp = now_num
+  n = 0
+  while temp > 0:
+    count[n] += temp % 2
+    n += 1
+    temp = temp // 2
+
+print(max(count))
