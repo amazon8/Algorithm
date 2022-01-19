800e8098d5ae5295ca706277b93399d339ac8462
yangbyeonghun <21900428@handong.edu>
2022-01-19 05:58:31 +0000

    수정

--- 알고리즘대회/인하대2021/b.py
+++ 알고리즘대회/인하대2021/b.py
@@ -12,35 +12,51 @@ for row in range(num):
 def dfs(row, col, count):
   global spot
   count += 1
-  right_count, down_count, diagonal_count = 0, 0, 0
+  right_count, left_count, up_count, down_count, diagonal_count, ru_diagonal_count, lu_diagonal_count, ld_diagonal_count = 0, 0, 0, 0, 0, 0, 0, 0
   if col != num-1 and spot[row][col+1] =="W":
     right_count = right(row, col+1, count)
-    print("오른쪽 끝 결과", right_count)
+  if col != 0 and spot[row][col-1] =="W":
+    left_count = left(row, col-1, count)
   if row != num-1 and spot[row+1][col] =="W":
     down_count = down(row+1, col, count)
+  if row != 0 and spot[row-1][col] =="W":
+    up_count = up(row-1, col, count)
   if col < num-1 and row < num-1 and spot[row+1][col+1] =="W":
     diagonal_count = diagonal(row+1, col+1, count)
+  if col < num-1 and row > 0 and spot[row-1][col+1] =="W":
+    ru_diagonal_count = ru_diagonal(row-1, col+1, count)
+  if col > 0 and row <num-1 and spot[row+1][col-1] =="W":
+    ld_diagonal_count = ld_diagonal(row-1, col-1, count)
+  if col > 0 and row > 0 and spot[row-1][col-1] =="W":
+    lu_diagonal_count = lu_diagonal(row+1, col+1, count)
   
-  print(right_count, down_count, diagonal_count)
-  return (right_count + down_count + diagonal_count)
+  return (right_count + left_count + down_count + up_count + diagonal_count + ru_diagonal_count + lu_diagonal_count + ld_diagonal_count)
 
 # 오른쪽 개수 확인
 def right(row, col, count):
-  print("abs")
   if col == num-1:
     return 0
   elif spot[row][col+1] == "W":
     count += 1
     return right(row, col+1, count)
   elif spot[row][col+1] == "B":
-    print("오른쪽 끝", count)
+    return count
+  else:
+    return 0
+# 왼쪽 개수
+def left(row, col, count):
+  if col == 0:
+    return 0
+  elif spot[row][col-1] == "W":
+    count += 1
+    return right(row, col-1, count)
+  elif spot[row][col-1] == "B":
     return count
   else:
     return 0
 
 # 밑으로 개수
 def down(row, col, count):
-  print("down")
   if row == num-1:
     return 0
   elif spot[row+1][col] == "W":
@@ -51,9 +67,21 @@ def down(row, col, count):
   else:
     return 0
 
+# 위로 개수
+def up(row, col, count):
+  if row == 0:
+    return 0
+  elif spot[row-1][col] == "W":
+    count += 1
+    return down(row-1, col, count)
+  elif spot[row-1][col] == "B":
+    return count
+  else:
+    return 0
+
+
 # 대각선 개수
 def diagonal(row, col, count):
-  print("댁각선")
   if row == num - 1 or col == num-1:
     return 0
   elif spot[row+1][col+1] == "W":
@@ -64,14 +92,54 @@ def diagonal(row, col, count):
   else:
     return 0
 
-res = []
+def ru_diagonal(row, col, count):
+  if row == 0 or col == num-1:
+    return 0
+  elif spot[row-1][col+1] == "W":
+    count += 1
+    return diagonal(row-1, col+1, count)
+  elif spot[row-1][col+1] == "B":
+    return count
+  else:
+    return 0
+
+def lu_diagonal(row, col, count):
+  if row == 0 or col == 0:
+    return 0
+  elif spot[row-1][col-1] == "W":
+    count += 1
+    return diagonal(row-1, col-1, count)
+  elif spot[row-1][col-1] == "B":
+    return count
+  else:
+    return 0
+
+def ld_diagonal(row, col, count):
+  if row == num - 1 or col == 0:
+    return 0
+  elif spot[row+1][col-1] == "W":
+    count += 1
+    return diagonal(row+1, col-1, count)
+  elif spot[row+1][col-1] == "B":
+    return count
+  else:
+    return 0
+
+
+res = [[0, 0, 0],
+       [0, 0, 0]] 
+
 # 판 전체 dfs로 탐색
 for row in range(num):
   for col in range(num):
     count = 0
     if spot[row][col] == ".":
       temp_res = dfs(row, col, count)
-      print("kkkk")
       res.append([temp_res, col, row])
 
-print(res)
+res.sort(reverse = True)
+if res[0][0] == 0:
+  print("PASS")
+else:
+  print(res[0][1], res[0][2])
+  print(res[0][0])
